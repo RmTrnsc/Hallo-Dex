ccdc9749e4ade42962fbfdcb77128b3b787f489f
bolbz <bourvaljonathan@gmail.com>
2018-10-30 22:59:20 +0100

    Mise en place du service 1

--- src/app/adress.service.ts
+++ src/app/adress.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdressService {
 
-  constructor() { }
+  private service:Http;
+
+  constructor(p_service:Http) {
+    this.service = p_service;
+   }
+
+   public getAdress(param_search:string):Observable<Array<string>> {
+
+    let url: string = "https://api-adresse.data.gouv.fr/search/";
+    url += "?q=" + param_search + "";
+    url += "&autocomplete=0";
+    url += "&type=street";
+
+    const obs1:Observable<Response>= this.service.get(url);
+
+    return obs1.pipe(
+      map(
+        (data:Response):string[] => {
+          let raw:any = data.json();
+          let features:Array<any> = raw.collection.features;
+          let i:number = features.length;
+          let adresse: Array<string> = new Array<string>();
+
+          console.log(features[0].properties);
+
+          while (--i > -1) {
+            adresse.push(features[i].properties[0].label);
+          }
+          return adresse;  
+        }
+      )
+    );
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { RouterModule} from '@angular/router';
 import { FormulaireComponent } from './formulaire/formulaire.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { AdressService } from './adress.service'
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { MapServiceService } from './map-service.service';
 import { CandyListComponent } from './candy-list/candy-list.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { CandyMapComponent } from './candy-map/candy-map.component';
+import { RecapitulatifComponent } from './recapitulatif/recapitulatif.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { CandyMapComponent } from './candy-map/candy-map.component';
     MainComponent,
     CandyListComponent,
     AcceuilComponent,
-    CandyMapComponent
+    CandyMapComponent,
+    RecapitulatifComponent
   ],
 
   imports: [
@@ -36,7 +38,7 @@ import { CandyMapComponent } from './candy-map/candy-map.component';
       {useHash: true}
     )
   ],
-  providers: [MapServiceService],
+  providers: [MapServiceService,AdressService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
